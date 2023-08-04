fcb4cc325e99e651657178c1c953d59f43b00628
Chris Edstrom <cedstrom@outlook.com>
2023-08-04 01:11:40 -0700

    Basic support for TSIG for RFC2136

--- src/tdnss/zone_api.py
+++ src/tdnss/zone_api.py
@@ -6,7 +6,19 @@ from dataclasses import dataclass
 
 from tdnss.connection import Connection
 
+from enum import auto
+from strenum import StrEnum
+
 log = logging.getLogger(__name__)
+
+
+class RFC2136Options(StrEnum):
+    Deny = auto()
+    Allow = auto()
+    AllowOnlyZoneNameServers = auto()
+    AllowOnlySpecifiedIpAddresses = auto()
+    AllowBothZoneNameServersAndSpecifiedIpAddresses = auto()
+
 
 @dataclass
 class ZoneResponse(BaseResponse):
@@ -54,7 +66,6 @@ class ZoneAPI:
             log.debug(self.connection._get_error_message(r))
             return ZoneResponse(ERROR, "Could not create zone")
 
-
     def delete_zone(self, zone: str):
         base_url = "zones/delete"
         params = {
@@ -64,10 +75,51 @@ class ZoneAPI:
         r = self.connection._get(base_url, params)
 
         if self.connection._is_ok(r):
-            resp = r.json().get("response")
             return ZoneResponse(OK)
 
         else:
             log.debug(f"{base_url=}, {r=}")
             log.debug(self.connection._get_error_message(r))
             return ZoneResponse(ERROR, "Could not delete zone")
+
+    def set_zone_options(self, zone, disabled=None, zone_transfer=None, zone_transfer_name_servers=None,
+                         zone_transfer_tsig_key_names=None, notify=None, notify_name_servers=None,
+                         update_policy=RFC2136Options.Deny,
+                         update_ip_addresses=None, update_tsig_key_name=None):
+
+        # Construct the URL for the API endpoint
+        endpoint_url = f"zones/options/set"
+
+        # Prepare the request parameters
+        params = {
+            "zone": zone,
+            "disabled": disabled,
+            "zoneTransfer": zone_transfer,
+            "zoneTransferNameServers": zone_transfer_name_servers,
+            "zoneTransferTsigKeyNames": zone_transfer_tsig_key_names,
+            "notify": notify,
+            "notifyNameServers": notify_name_servers,
+            "update": update_policy,
+            "updateIpAddresses": update_ip_addresses,
+        }
+
+        # `updateSecurityPolicies` (optional): A pipe `|` separated table data of security policies
+        # with each row containing the TSIG keys name, domain name, and comma separated record types
+        # that are allowed. Use wildcard domain name to specify all sub domain names. Set this
+        # option to `false` to clear all security policies and stop TSIG authentication.
+        # This option is valid only for Primary zones.
+        if update_tsig_key_name is not None:
+            params["updateSecurityPolicies"] = f'{update_tsig_key_name}|{zone}|any'
+
+        params = {k:v for (k,v) in params.items() if v is not None}
+
+        r = self.connection._get(endpoint_url, params)
+
+        if self.connection._is_ok(r):
+            resp = r.json().get("response")
+            return ZoneResponse(OK, data=resp)
+
+        else:
+            log.debug(f"{endpoint_url=}, {r=}")
+            log.debug(self.connection._get_error_message(r))
+            return ZoneResponse(ERROR, self.connection._get_error_message(r))
--- test/zone_test.py
+++ test/zone_test.py
@@ -1,7 +1,10 @@
+import string
 import unittest
+import random
 
 from tdnss import OK
 from tdnss.connection import Connection
+from tdnss.zone_api import RFC2136Options
 
 
 class ZoneTests(unittest.TestCase):
@@ -17,6 +20,23 @@ class ZoneTests(unittest.TestCase):
         r = connection.zone_api().delete_zone("example.com")
         self.assertEqual(r.status, OK)
 
+    def test_enable_allow_zone_updates(self):
+        zone_name = f'test-{"".join(random.sample(string.ascii_lowercase[:26], 16))}.com'
+
+        connection = Connection()
+        r = connection.login()
+        self.assertEqual(r.status, OK)
+
+        r = connection.zone_api().create_zone(zone_name)
+        self.assertEqual(r.status, OK)
+
+        r = connection.settings_api().add_tsig_key(zone_name)
+        self.assertEqual(r.status, OK)
+
+        r = connection.zone_api().set_zone_options(zone_name,
+                                                   update_policy=RFC2136Options.Allow,
+                                                   update_tsig_key_name=zone_name)
+        self.assertEqual(r.status, OK)
 
 if __name__ == '__main__':
     unittest.main()
